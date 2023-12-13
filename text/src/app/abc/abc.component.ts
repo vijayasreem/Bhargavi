e945b596151c2463498f3eb1a54f304ba33c57fe
dotnet-code <@dotnet-code>
2023-12-13 08:55:23 +0000

    commit from code202312135523

---
+++ text/src/app/abc/abc.component.ts
@@ -0,0 +1,36 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-abc',
+  templateUrl: './abc.component.html',
+  styleUrls: ['./abc.component.css']
+})
+export class AbcComponent implements OnInit {
+  transactionForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.transactionForm = this.formBuilder.group({
+      creditAmount: ['', [Validators.required, Validators.min(0), Validators.max(50000)]]
+    });
+  }
+
+  submitTransaction(): void {
+    if (this.transactionForm.invalid) {
+      return;
+    }
+
+    // Call API service to submit transaction
+    this.apiService.submitTransaction(this.transactionForm.value).subscribe(
+      response => {
+        // Handle success response
+      },
+      error => {
+        // Handle error response
+      }
+    );
+  }
+}
